cd29e27d027c46de514a56cc42ef05b94fbf2ac6
bhuvanarongala <96564246+bhuvanarongala@users.noreply.github.com>
2022-01-07 22:17:24 -0800

    Add files via upload

--- prime.c
+++ prime.c
@@ -3,19 +3,21 @@ main()
 {
 	int i,n,fc=0;
 	scanf("%d",&n);
-	for(i=1; i<=n; i++)
+	for(i=2; i<=n/2; i++)
 	{
 		if(n%i==0)
 		{
-		fc++;
-	    }
-   }
-   if(fc==2)
-   {
-   	printf("prime");
-   }
-   else
-   {
-   	printf("not prime");
-   }
+			fc++;
+			
+		}
+	}
+	if(fc==0)
+	{
+		printf("Prime");
+	}
+	else
+	{
+		printf("Not Prime");
+	}
 }
+
